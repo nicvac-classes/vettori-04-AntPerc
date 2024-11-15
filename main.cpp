d76bacddc13461687269ceb50437ed809d450319
AntPerc <antonio.percuoco@marconibari.edu.it>
2024-11-15 15:54:55 +0000

    ho concluso

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int n, i, min, imin, max, imax, contPari;
+    cout << "Quanti numeri casuali dobbiamo generare?" << endl;
+    cin >> n;
+    int numeri[n];
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    for (i = 0; i <= n - 1; i++) {
+        numeri[i] = random() % 1001;
+        cout << i + 1 << "Numero: " << numeri[i] << endl;
+    }
+    cout << "Calcolo di quanti numeri pari sono stati generati..." << endl;
+    contPari = 0;
+    for (i = 0; i <= n - 1; i++) {
+        if (numeri[i] % 2 == 0) {
+            contPari = contPari + 1;
+        }
+    }
+    cout << "Sono stati generati: " << contPari << "numeri pari" << endl;
+    cout << "Calcolare il numero minore e il numero maggiore..." << endl;
+    min = numeri[0];
+    imin = 0;
+    max = numeri[0];
+    imax = 0;
+    for (i = 1; i <= n - 1; i++) {
+        if (numeri[i] < min) {
+            min = numeri[i];
+            imin = i;
+        }
+        if (numeri[i] > max) {
+            max = numeri[i];
+            imax = i;
+        }
+    }
+    cout << "Il valore minimo è: " << min << " in posizione " << imin << endl;
+    cout << "Il valore massimo è: " << max << " in posizione " << imax << endl;
+    return 0;
+}
